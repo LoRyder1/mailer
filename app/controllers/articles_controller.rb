0ff0362f5cb707acbb703390be942eaaf4ec55a4
Lovinder Pnag <lspnag@gmail.com>
2017-01-18 08:58:56 -0800

    working resqueue monitor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(params[:article])
 
     if @article.save
-      sleep(5)
+      Resqueue.enqueue(Sleeper, 5)
 
       flash[:notice] = "Article was created."
       redirect_to article_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+require 'resque/server'
+
 Rails.application.routes.draw do
   resources :articles
+
+  mount Resque::Server.new, at: "/resque"
 end
